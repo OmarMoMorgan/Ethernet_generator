948ea941d86fc1fb81d71ca04c27d7d54e15cf35
OmarMoMorgan <s-omar.morgan@zewailcity.edu.eg>
2024-09-24 21:51:37 +0300

    added the ecpri packet

--- Project1/EcpriPacket.cpp
+++ Project1/EcpriPacket.cpp
@@ -1,1 +1,31 @@
 #include "EcpriPacket.h"
+
+
+EcpriPacket::EcpriPacket() {
+	headerSize = 8;
+}
+
+std::vector<uint8_t> EcpriPacket::GetEcpri(std::vector<uint8_t> &ORANpack) {
+	int payloadSize = ORANpack.size();
+	
+	std::vector<uint8_t> ecpriPacket(headerSize+payloadSize,payloadSize);
+	//just add the payload here this solution is really stupid for now
+
+	for (int i = 0; i < payloadSize; i++) {
+		ecpriPacket[8 + i] = ORANpack[i];
+	}
+	return ecpriPacket;
+}
+
+
+std::vector<uint8_t> EcpriPacket::addEcpriHeader(std::vector<uint8_t>& emptyEcpri,int payloadSize) {
+	emptyEcpri[0] = 0; //this was asked in requirments for now
+	emptyEcpri[1] = 0; //this is the message type we are assuming it is u plane
+	emptyEcpri[2] = (payloadSize >> 8) & 0xff;
+	emptyEcpri[3] = payloadSize & 0xff;
+	emptyEcpri[4] = 0; //also for now fixed as zero
+	emptyEcpri[5] = 0; //also for now fixed as zero
+	emptyEcpri[6] = seqID;
+	emptyEcpri[7] = 0;
+	seqID++;
+}
--- Project1/EcpriPacket.h
+++ Project1/EcpriPacket.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <vector>
 class EcpriPacket
 {
+
+private:
+	static int seqID;
+	int headerSize;
+
+public:
+	EcpriPacket();
+	std::vector<uint8_t> GetEcpri(std::vector<uint8_t> &ORANpack); //this function will take a vector and convert it to ecpri
+	std::vector<uint8_t> addEcpriHeader(std::vector<uint8_t>& emptyEcpri, int payloadSize);
+
 };
 
